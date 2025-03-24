32796e99e8b6c75bdf630a73e1132980851752e0
crobr <cristiana.obrien@uhn.ca>
2025-03-24 18:44:11 -0400

    Add files via upload

---
+++ R_pruning.R
@@ -0,0 +1,27 @@
+
+# Clean global environment
+rm(list = ls())
+
+#Libraries
+library(survival)
+library(survminer)
+library(M3C)
+
+#work computer
+setwd('/Users/crobrien/OneDrive - UHN/Jones Lab/3. Lipids-Relapse AML/2. Plasma Sample Lipidomics Data/New folder/Final Data/Survival')
+
+
+# df <- read.csv("Clinical_met_noMDS_Final.csv")
+df<-read.csv("Clinical_lip_noMDS_Final.csv")
+
+sfit<-survfit(Surv(time_OS, status)~1,data=df)
+
+ggsurvplot(sfit)
+
+
+##filter data based on variability 
+lip_log2 <-t(subset (df, select=-c(1:2)))
+filtered<-featurefilter(lip_log2, percentile=5, method = "var",topN=105)
+idx<-data.frame(filtered$filtered_data)
+rows<- rownames(idx)
+top100<-df[rows]
